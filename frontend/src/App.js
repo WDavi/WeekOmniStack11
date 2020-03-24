bf1d64dea862674b54788a93820cfcaeae586b9e
Davi Wendt <daviwendt@gmail.com>
2020-03-24 13:30:14 -0300

    Primeiro Dia Final

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo11.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Semana OmniStack 11.0
+        </p>
+        <a
+          className="App-link"
+          href="https://rocketseat.com.br/week/inscricao/11.0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn OmniStack
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
